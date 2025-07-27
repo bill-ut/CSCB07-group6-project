6bb05085d0bff94ed4f6a79623fdfd99912b8d82
Shengtao Liu <lshengtao05@gmail.com>
2025-07-27 15:26:07 -0400

    Add Emergency Info class structure

---
+++ Starter Application/app/src/main/java/com/example/b07demosummer2024/emergency/EmergencyContact.java
@@ -0,0 +1,17 @@
+package com.example.b07demosummer2024.emergency;
+
+public class EmergencyContact {
+    public String name;
+    public String relationship;
+    public String phone;
+
+    public EmergencyContact() {
+    }
+
+    public EmergencyContact(String name, String relationship, String phone) {
+        this.name = name;
+        this.relationship = relationship;
+        this.phone = phone;
+    }
+}
+
